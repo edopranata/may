ea8fdfe3be1dac39c1b0e3bbc62bd64e01ce1de8
Edo Pranata <edo.pranata@gmail.com>
2022-11-13 18:39:38 +0700

    Disable transaction seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
             DriverSeeder::class,
             SupervisorSeeder::class,
             ConfigurationSeeder::class,
-            TradeSeeder::class,
-            UpdateTradeSeeder::class,
+//            TradeSeeder::class,
+//            UpdateTradeSeeder::class,
         ]);
     }
 }
